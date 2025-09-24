316139ce35d1deaf4efc494555d5e67719cbae6c
trigunraisharma <trigunraisharma@gmail.com>
2025-09-24 11:04:57 +0000

    1

--- infra-1/bootstrap/backend.tf
+++ infra-1/bootstrap/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "my-react-node-app-terraform-state-bootstrap" # your S3 bucket
-    key     = "terraform.tfstate"                           # path inside the bucket
+    key     = "infra_plus_bootstrap/terraform.tfstate"                           # path inside the bucket
     region  = "us-east-1"
     encrypt = true
   }
