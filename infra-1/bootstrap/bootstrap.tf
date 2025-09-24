c1690700bbbca8396bec4c7f813930f5a4e478af
trigunraisharma <trigunraisharma@gmail.com>
2025-09-24 13:51:45 +0000

    3

--- infra-1/bootstrap/bootstrap.tf
+++ infra-1/bootstrap/bootstrap.tf
@@ -3,18 +3,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_state" {
-  provider = aws.bootstrap
-  bucket   = "my-react-node-app-bucket02"
-}
+#resource "aws_s3_bucket" "tf_state" {
+  #provider = aws.bootstrap
+  #bucket   = "my-react-node-app-bucket02"
+#}
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
-  provider = aws.bootstrap
-  bucket   = "my-react-node-app-bucket02"
+#resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  #provider = aws.bootstrap
+  #bucket   = "my-react-node-app-bucket02"
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+  #rule {
+    #apply_server_side_encryption_by_default {
+      #sse_algorithm = "AES256"
+    #}
+  #}
+#}
